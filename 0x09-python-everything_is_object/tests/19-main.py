73e6404063f8a62bd4ede8c220370cad8dcfabbd
salah-barkai <salahbarkai12@gmail.com>
2024-02-05 23:18:25 -0800

    python everythings

---
+++ 0x09-python-everything_is_object/tests/19-main.py
@@ -0,0 +1,14 @@
+19-main.py
+#!/usr/bin/python3
+copy_list = __import__('19-copy_list').copy_list
+
+my_list = [1, 2, 3]
+print(my_list)
+
+new_list = copy_list(my_list)
+
+print(my_list)
+print(new_list)
+
+print(new_list == my_list)
+print(new_list is my_list)
